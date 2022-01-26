e18443b28423e961ff6bb5bb1515164f286ae342
wc1c-ceo <ceo@wc1c.info>
2022-01-26 19:47:29 +0300

    Fix: more

--- src/Core.php
+++ src/Core.php
@@ -26,7 +26,84 @@ final class Core extends ExtensionAbstract
 	 */
 	public function initHooks()
 	{
+		if(wc1c()->context()->isAdmin('plugin'))
+		{
+			add_filter(WC1C_PREFIX . 'configurations-update_form_load_fields', [$this, 'configurations_fields_other'], 150, 1);
+		}
 
+		if(wc1c()->context()->isReceiver())
+		{
+			add_filter(WC1C_PREFIX . 'schema_productscml_receiver_send_response_by_type_description', [$this, 'filter_send_response_by_type_description'], 10, 3);
+			add_filter(WC1C_PREFIX . 'schema_productscml_receiver_send_response_by_type_headers', [$this, 'filter_send_response_by_type_headers'], 10, 3);
+		}
+	}
+
+	/**
+	 * @param $description
+	 * @param $context
+	 * @param $type
+	 *
+	 * @return string
+	 */
+	public function filter_send_response_by_type_description($description, $context, $type)
+	{
+		if(!empty($description) && $context->core()->getOptions('support_1251', 'no') === 'yes')
+		{
+			$description = mb_convert_encoding($description, 'cp1251', 'utf-8');
+			$context->core()->log()->info('Response charset converted to Windows-1251.', ['description' => $description]);
+		}
+
+		return $description;
+	}
+
+	/**
+	 * @param $headers
+	 * @param $context
+	 * @param $type
+	 *
+	 * @return string
+	 */
+	public function filter_send_response_by_type_headers($headers, $context, $type)
+	{
+		if($context->core()->getOptions('support_1251', 'no') === 'yes')
+		{
+			$headers['Content-Type'] = 'Content-Type: text/plain; charset=Windows-1251';
+		}
+
+		return $headers;
+	}
+
+	/**
+	 * Configuration fields: other
+	 *
+	 * @param $fields
+	 *
+	 * @return array
+	 */
+	public function configurations_fields_other($fields)
+	{
+		$this->arrayInsert($fields, 'php_post_max_size', $this->getConfigurationFields());
+
+		return $fields;
+	}
+
+	/**
+	 * Configuration fields
+	 *
+	 * @return array
+	 */
+	public function getConfigurationFields()
+	{
+		$fields['support_1251'] =
+		[
+			'title' => __('Поддержка Windows-1251', 'wc1c-support-1251'),
+			'type' => 'checkbox',
+			'label' => __('Отметьте чекбокс, если необходимо включить данную возможность. По умолчанию выключена.', 'wc1c-support-1251'),
+			'description' => __('При включении настройки, данные из кодировки utf-8 будут конвертироваться в кодировку Windows-1251. Следует использовать для совместимости со старыми версиями 1С, которые не поддерживают современные кодировки.', 'wc1c-support-1251'),
+			'default' => 'no'
+		];
+
+		return $fields;
 	}
 
 	/**
@@ -66,7 +143,7 @@ final class Core extends ExtensionAbstract
 		}
 		else
 		{
-			$pos = array_search($position, array_keys($array), true);
+			$pos = array_search($position, array_keys($array), true) + 1;
 
 			$array = array_merge
 			(
